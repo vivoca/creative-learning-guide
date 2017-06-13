77fd5191ea57aabe152e9b2b80be3e2d765bc560
egyud_v <vivien.egyud@gmail.com>
2017-06-13 12:48:49 +0200

    SpringBoot works

--- src/main/java/codecool/learningguide/Main.java
+++ src/main/java/codecool/learningguide/Main.java
@@ -1,7 +1,20 @@
 package codecool.learningguide;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
     }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
