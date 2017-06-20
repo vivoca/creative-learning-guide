e7df1e3c23a5a98e07c093f0d7ea1a83d3514b2e
egyud_v <vivien.egyud@gmail.com>
2017-06-20 11:13:47 +0200

    html for all words in database

--- src/main/java/codecool/learningguide/Controller/QuestionController.java
+++ src/main/java/codecool/learningguide/Controller/QuestionController.java
@@ -1,12 +1,13 @@
 package codecool.learningguide.Controller;
 
+import codecool.learningguide.model.Translation;
+import codecool.learningguide.service.ExerciseService;
 import codecool.learningguide.service.TranslationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class QuestionController {
@@ -14,14 +15,9 @@ public class QuestionController {
     @Autowired
     TranslationService translationService;
 
-    @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
 
     @RequestMapping(value = "/getAllWords", method = RequestMethod.GET)
-    public String getAllEmployees(Model model) {
+    public String getAllTranslation(Model model) {
         model.addAttribute("translations", translationService.getAllTranslation());
         return "getAllWords";
     }
