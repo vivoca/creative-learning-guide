e1307f151124ec2893704718cc53559b690cd08e
egyud_v <vivien.egyud@gmail.com>
2017-06-20 15:54:20 +0200

    csv filehandling temporary solution, WORKS

--- src/main/java/codecool/learningguide/Main.java
+++ src/main/java/codecool/learningguide/Main.java
@@ -1,6 +1,13 @@
 package codecool.learningguide;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +22,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
 
     @RequestMapping("/")
@@ -25,8 +31,28 @@ public class Main extends SpringBootServletInitializer {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Main.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job job;
+
+    @RequestMapping("/runjob")
+    public String handle() throws Exception {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        try {
+            JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            jobLauncher.run(job, jobParameters);
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        return "Done! Check Console Window for more details";
     }
 
 }
